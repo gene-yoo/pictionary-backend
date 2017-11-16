af4007c21773a0654a27a889d0bf438e42bd3189
rebeccanewborn <rjn58@cornell.edu>
2017-11-16 12:14:39 -0500

    more latest comments

--- app/models/game.rb
+++ app/models/game.rb
@@ -17,7 +17,7 @@ class Game < ApplicationRecord
   end
 
   def recentMessages
-    self.messages.last(3).map do |msg|
+    self.messages.last(8).map do |msg|
       { player_username: msg.player_username, content: msg.content, player_id: msg.player_id }
     end
   end
